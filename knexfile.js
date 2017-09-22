ed4d3a0496738a1cdef98c81ec07a35c3208a112
Elizabeth <ehuntsman@wavetronix.com>
2017-09-22 16:06:00 -0600

    setup for react

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,15 @@
 module.exports = {
-    client: 'sqlite3',
+    client: 'mysql',
     connection: {
-      filename: process.env.TESTING ? "./tests/e2e/db/matterwiki.sqlite" : "./db/matterwiki.sqlite"
+      host : 'us-cdbr-iron-east-05.cleardb.net',
+      user : 'b7904d2ddd5c09',
+      password : '82a3315e',
+      database : 'heroku_c83e728a5fec512'
+      // filename: process.env.TESTING ? "./tests/e2e/db/matterwiki.sqlite" : "./db/matterwiki.sqlite"
     },
     useNullAsDefault: true
   }
+
 
 /*
 The development object is the connection object for the development database.
